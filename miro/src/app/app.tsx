c26bbfd26a981311297c882edf5b7974ca1a4d92
Vasilkova Liya <koodoo@bk.ru>
2026-02-11 18:17:27 +0300

    add structure + eslint config

---
+++ miro/src/app/app.tsx
@@ -0,0 +1,4 @@
+import { BoardPage } from "@/features/board";
+export function App() {
+  return <BoardPage />;
+}
---
+++ miro/src/features/boards-list/index.tsx
@@ -0,0 +1,8 @@
+import BoardCard from "./board-card";
+export function BoardsListPage() {
+  return (
+    <div>
+      <BoardCard />
+    </div>
+  );
+}
