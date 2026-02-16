32ddf96478033f11c0500bc2b05696f017f401cb
Vasilkova Liya <koodoo@bk.ru>
2026-02-16 15:25:36 +0300

    add env

--- miro/src/features/boards-list/boards-list.page.tsx
+++ miro/src/features/boards-list/boards-list.page.tsx
@@ -1,10 +1,13 @@
 import { ROUTES } from "@/shared/model/routes";
 import { Link, href } from "react-router-dom";
+import { CONFIG } from "@/shared/config";
 
 function BoardsListPage() {
+  console.log(import.meta.env.VITE_API_URL);
   return (
     <div>
       <h1>Boards list</h1>
+      <div>CONFIG.VITE_API_BASE_URL: {CONFIG.VITE_API_BASE_URL} </div>
       <Link to={href(ROUTES.BOARD, { boardId: "1" })}>Board 1</Link>
     </div>
   );
