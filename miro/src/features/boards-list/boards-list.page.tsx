0ed4ee9479475f5b2579e7c8f344aec1ec4fe971
Vasilkova Liya <koodoo@bk.ru>
2026-02-17 08:29:28 +0300

    add api (openapi-fetch, openapi-react-query, openapi-msw),  React Quer // заложена функциональность создания удаления досок
    настроены типизированный API по OpenAPI (openapi-fetch + openapi-react-query), MSW-моки (openapi-msw), запуск воркера в dev до рендера, общий React Query-клиент и провайдер, а на странице списка досок — загрузка списка, создание, удаление и обновление списка через инвалидацию кэша после мутаций

--- miro/src/features/boards-list/boards-list.page.tsx
+++ miro/src/features/boards-list/boards-list.page.tsx
@@ -1,15 +1,59 @@
 import { ROUTES } from "@/shared/model/routes";
 import { Link, href } from "react-router-dom";
-import { CONFIG } from "@/shared/config";
+import { CONFIG } from "@/shared/model/config";
+import { rqClient } from "@/shared/api/instance";
+import { useQueryClient } from "@tanstack/react-query";
 
 function BoardsListPage() {
-  console.log(import.meta.env.VITE_API_URL);
+  console.log(CONFIG.API_BASE_URL)
+
+  const queryClient = useQueryClient();
+  const boardsQuery = rqClient.useQuery('get', '/boards');
+  const createBoardMutation = rqClient.useMutation('post', '/boards', {
+    onSettled: async () => {
+      await queryClient.invalidateQueries(rqClient.queryOptions('get', '/boards'));
+    },
+  });
+  const deleteBoardMutation = rqClient.useMutation('delete', '/boards/{boardId}', {
+    onSettled: async () => {
+      await queryClient.invalidateQueries(rqClient.queryOptions('get', '/boards'));
+    },
+  });
   return (
     <div>
       <h1>Boards list</h1>
-      <div>CONFIG.VITE_API_BASE_URL: {CONFIG.VITE_API_BASE_URL} </div>
-      <Link to={href(ROUTES.BOARD, { boardId: "1" })}>Board 1</Link>
-    </div>
+      <form action=""
+           onSubmit={(e) => {
+            e.preventDefault();
+            const formData = new FormData(e.target as HTMLFormElement);
+            const name = formData.get('name') as string;
+             createBoardMutation.mutate({ body: { name } });
+            e.target.reset();
+          }}
+      >
+        <input type="text" name="name" />
+        <button type="submit" disabled={createBoardMutation.isPending}>
+          Create Board
+        </button>
+      </form>
+      {boardsQuery.data?.map((board) => (
+        <div>
+          <Link to={href(ROUTES.BOARD, { boardId: board.id })}>{board.name}_{board.id}</Link>
+          <button
+            disabled={deleteBoardMutation.isPending}
+            onClick={() => 
+            deleteBoardMutation.mutate({ 
+              params: { path: { boardId: board.id }},
+            })
+          }
+            >
+              Delete Board
+          </button>
+        </div>
+      ))}
+      <div>CONFIG.API_BASE_URL: {CONFIG.API_BASE_URL} </div>
+      <button onClick={() => createBoardMutation.mutate({ body: { name: "New Board" } })}>Create Board</button>
+      </div>
   );
 }
 
